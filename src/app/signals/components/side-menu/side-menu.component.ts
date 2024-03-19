67ca30cf8d5f2f2cff423720131b014df95a9d3f
Patricia Manzanero Jiménez <52755910+Ossyyrr@users.noreply.github.com>
2024-03-19 12:54:06 +0100

    wip

--- src/app/signals/components/side-menu/side-menu.component.ts
+++ src/app/signals/components/side-menu/side-menu.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 
 interface MenuItem {
   title: string;
@@ -11,7 +11,8 @@ interface MenuItem {
   styleUrl: './side-menu.component.css',
 })
 export class SideMenuComponent {
-  public menuItems: MenuItem[] = [
+  public menuItems = signal<MenuItem[]>([
+    //! signal sirve para que el componente se actualice cuando cambie el valor
     {
       title: 'Contador',
       router: 'counter',
@@ -24,5 +25,20 @@ export class SideMenuComponent {
       title: 'Mutaciones',
       router: 'properties',
     },
-  ];
+  ]);
+
+  // public menuItems: MenuItem[] = [
+  //   {
+  //     title: 'Contador',
+  //     router: 'counter',
+  //   },
+  //   {
+  //     title: 'Usuario',
+  //     router: 'user-info',
+  //   },
+  //   {
+  //     title: 'Mutaciones',
+  //     router: 'properties',
+  //   },
+  // ];
 }
--- src/app/signals/page/counter-page/counter-page.component.ts
+++ src/app/signals/page/counter-page/counter-page.component.ts
@@ -1,8 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, computed, signal } from '@angular/core';
 
 @Component({
   selector: 'app-counter-page',
   templateUrl: './counter-page.component.html',
   styleUrl: './counter-page.component.css',
 })
-export class CounterPageComponent {}
+export class CounterPageComponent {
+  public counter = signal(10);
+  public squareCounter = computed(() => this.counter() ** 2); //! SOLO LECTURA. Cuando cambia la señal counter el computed() vuelve a computar y se actualiza en todos los lugares donde se está utilizando
+
+  increaseBy(value: number) {
+    this.counter.update((prev) => prev + value);
+  }
+}
